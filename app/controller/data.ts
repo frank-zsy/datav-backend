25a2d05397e233d56fc2f9eca40f38fb20ef66df
Frankzhaopku <syzhao1988@126.com>
2020-10-20 15:22:03 +0800

    refactor: legend size and bg fix
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

--- app/controller/data.ts
+++ app/controller/data.ts
@@ -405,8 +405,8 @@ export default class DataController extends Controller {
     // let svgInnerContent = '<rect x="0" y="0" width="400" height="140" style="fill:#41495C99" />';
     let svgInnerContent = '';
     projects.forEach((project, index) => {
-      svgInnerContent += `<rect x="${30 + (index % 2) * 230}" y="${15 + (Math.floor(index / 2)) * 40}" width="30" height="30" fill="${colors[index]}" />`;
-      svgInnerContent += `<rect x="${60 + (index % 2) * 230}" y="${15 + (Math.floor(index / 2)) * 40}" width="170" height="30" fill="#000000CC" />`
+      svgInnerContent += `<rect x="${30 + (index % 2) * 230}" y="${15 + (Math.floor(index / 2)) * 40}" width="200" height="30" fill="#000000CC" />`
+      svgInnerContent += `<rect x="${32 + (index % 2) * 230}" y="${17 + (Math.floor(index / 2)) * 40}" width="26" height="26" fill="${colors[index]}" />`;
       svgInnerContent += `<text x="${145 + (index % 2) * 230}" y="${30 + (Math.floor(index / 2)) * 40}">${project.name}</text>`;
     });
 
