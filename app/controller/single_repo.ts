452092158d7b7818aad063e56bb64d4ae5bb1f03
frank-zsy <syzhao1988@126.com>
2022-07-07 23:08:29 +0800

    add default repo name
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- app/controller/single_repo.ts
+++ app/controller/single_repo.ts
@@ -2,16 +2,20 @@ import { Controller } from 'egg';
 
 export default class SingleRepoController extends Controller {
 
+  private getName() {
+    let name = this.ctx.query.r;
+    if (!name || name === ':r') name = 'x-lab2017/open-digger';
+    return name;
+  }
+
   private async process(type: string) {
-    const name = this.ctx.query.r;
-    if (!name) return this.ctx.body = [{ value: 'Repo not set' }];
+    const name = this.getName();
     const url = `${this.app.config.datav.ossUrl}single_repo_dashboard/${name.toLowerCase()}/${type}.json`;
     this.ctx.body = (await this.app.curl(url, { dataType: 'json' })).data;
   }
 
   public title() {
-    const name = this.ctx.query.r;
-    if (!name) return this.ctx.body = [{ value: 'Repo not set' }];
+    const name = this.getName();
     this.ctx.body = [{ value: `${name} 项目数据大屏` }];
   }
 
